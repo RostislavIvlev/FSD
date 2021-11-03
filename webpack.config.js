a43c5939473d92dc485bb547f0a411fc634f021a
RostislavIvlev <56726375+RostislavIvlev@users.noreply.github.com>
2021-11-04 00:23:12 +0500

    Шрифты Подключение шрифтов Переменные (шрифты, цвета, размеры экрана) Изменения вебпака (для загрузки шрифтов в отдельную папку при продакте/деве) Подключение шрифтов и переменных в main.scss

--- webpack.config.js
+++ webpack.config.js
@@ -90,13 +90,19 @@ module.exports = {
             },
 
             {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,
                 type: 'asset/resource',
+                generator: {
+                    filename: 'assets/images/[name][ext][query]'
+                }
             },
 
             {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
                 type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[name][ext][query]'
+                }
             },
 
             {
